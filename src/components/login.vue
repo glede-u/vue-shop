99779a074d14e1f0718d32740c5eaf23c82f66b6
glede-u <1277032954@qq.com>
2021-01-27 18:20:35 +0800

    login验证

---
+++ src/components/login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/mi.png" alt="" />
+      </div>
+      <!-- from表单 -->
+      <el-form ref="loginFormRefs" :model="loginForm" :rules="loginRules" class="form_box">
+        <!-- 登录 -->
+        <el-form-item prop="username" >
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+         <el-form-item  prop="password">
+          <el-input v-model="loginForm.password" type="password" prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+         <el-form-item class="btns">
+            <el-button type="primary">登录</el-button>
+            <el-button type="info" @click="resetLoginFrom">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+
+  data() {
+    return {
+      loginForm:{
+        username:'lis',
+        password:'000000'
+      },
+      loginRules:{
+        username:[
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 2, max: 17, message: '长度在 2 到 17 个字符', trigger: 'blur' }
+        ],
+        password:[
+           { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 21, message: '长度在 6 到 21 个字符', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+
+  methods: {
+    resetLoginFrom(){
+      this.$refs.loginFormRefs.resetFields()
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+};
+.form_box{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 20px;
+ 
+}
+.btns{
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   devServer: {
     open: true,
     port: 8888
-  }
+  },
+  // 禁止eEslist使用
+  // lintOnSave:false
 }
